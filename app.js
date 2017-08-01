c65cd7cc860fe46c45a678dba9439ded072f8929
Ben Kielty <bwk103@gmail.com>
2017-08-01 13:01:01 +0100

    Create backend routes for create / read plants
    Added connection to mongoose, added  basic model for plants. Created
    '/plants' get and post routes.

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var path = require('path');
 var http = require('http');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//mongoose
+
+mongoose.connect('mongodb://localhost/planter');
+
+//express config
 
 const app = express();
 
@@ -10,6 +17,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+//routes
+
 const appRoutes = require('./routes/app')
 
 app.use('/', appRoutes);
@@ -17,6 +26,8 @@ app.use('/', appRoutes);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
+//server
 
 const port = process.env.PORT || '3000';
 
--- routes/app.js
+++ routes/app.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var Plant = require('../models/plant');
 
 router.get('/', (req, res) => {
-  res.send('This is the home page');
+  res.send('This will be the home page');
 })
 
+router.get('/plants', (req, res, next) => {
+  Plant.find({}, (error, plants) => {
+    if (error) {
+      return res.sattus(501).json({
+        title: 'An error occured',
+        error: error
+      })
+    } else {
+      res.status(200).json({
+        message: 'Plants successfully found',
+        object: plants
+      });
+    }
+  });
+});
+
+router.post('/plants', (req, res, next) => {
+  var newPlant = new Plant({
+    name: req.body.name,
+    image: req.body.image
+  });
+  Plant.create(newPlant, (error, plant) => {
+    if (error) {
+      return res.status(501).json({
+        title: 'An error occured',
+        error: error
+      });
+    } else {
+      res.status(200).json({
+        message: 'Plant successfully added to database',
+        object: plant
+      });
+    }
+  })
+});
 
 module.exports = router;
